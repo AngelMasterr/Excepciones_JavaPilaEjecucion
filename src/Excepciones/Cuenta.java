0f6c54abe519ec95ff6844e8da05e912bd1b2d6d
AngelMasterr <angelduarteprog@gmail.com>
2023-07-14 19:40:41 -0500

    mas

--- src/Excepciones/Cuenta.java
+++ src/Excepciones/Cuenta.java
@@ -1,10 +1,13 @@
 package Excepciones;
 
+import java.lang.reflect.Constructor;
+
 public class Cuenta {
 	
 	public void deposita() throws MiException{
 		double a = 50/0;
 		System.out.println(a);
 	}
+	
 	
 }
